a2bb34f0a8caf6aec220736f6795c0ac04d94ba8
deytapas <deytapas374@gmail.com>
2025-07-26 12:00:09 +0530

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return <div className='px-5 py-6 bg-red-300'>
+    <span className='text-red-50'>Hellow World !</span>
+  </div>
+}
+
+export default App
